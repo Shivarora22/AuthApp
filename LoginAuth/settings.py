367cad578a520e14d5941d628171a2d5d0507708
Shiv Arora <104933581+Shivarora22@users.noreply.github.com>
2024-10-31 14:45:10 +0530

    Update settings.py

--- LoginAuth/settings.py
+++ LoginAuth/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'coe211231.cse.coe@cgc.edu.in'
-EMAIL_HOST_PASSWORD = 'sanjay@#parivar'
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
